97f6a08a02b98f8cdd361753472605eee5a7f944
Dennis lam <denwt.lam@outlook.com>
2021-08-02 16:04:14 -0400

    play with http api requests

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-    console.log('2 Second Timer');
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=0a9275d4360d2bc6950b120763fbede6&query=37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping');
+request({ url:url, json: true }, (error, response) => {
+    console.log(response.body.current);
+    console.log("It is currently " + response.body.current.temperature+ " degrees out. It feels like " + response.body.current.feelslike+ " degrees out.");
+});
