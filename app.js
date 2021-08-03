9562efc85f74e6f6a3da9048d689877501d3efec
Dennis lam <denwt.lam@outlook.com>
2021-08-03 12:00:25 -0400

    chain asynchronous request callbacks of weather and forecast

--- app.js
+++ app.js
@@ -1,42 +1,15 @@
 const request = require('request');
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
 
-// const url = 'http://api.weatherstack.com/current?access_key=0a9275d4360d2bc6950b120763fbede6&query=37.8267,-122.4233&units=m';
 
-// request({ url:url, json: true }, (error, response) => { //if there is an error there is no response and vice versa
+geocode('Boston', (error, data) => {
+    console.log("Error:", error);
+    console.log("Data:", data);
 
-//     if(error){
+    forecast(data.latitude, data.longitude, (error, data) => {
+        console.log('Error', error)
+        console.log('Data', data)
+    })
 
-//         console.log('Unable to connect to weather service!');
-
-//     }else if(response.body.error){
-        
-//         console.log('Unable to find location');
-    
-//     }else{
-//         // console.log(response.body.current);
-//         console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature+ " degrees out. It feels like " + response.body.current.feelslike+ " degrees out.");  
-//     }
-
-// });
-
-//Geocoding
-//Address -> Lat/Long -> Weather
-
-const url2 = "https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiZnVuaW5qYXBwbGUiLCJhIjoiY2tydjJnZDB3MDJrNDJvcjBxZDI3M3IzbyJ9.XJkezJUKp0v3g6w7qXyWEg&limit=1"
-
-request({url: url2, json:true}, (error, response) => {
-
-    if (error){
-        console.log("Unable to connect to network");
-
-    }else if(response.body.features.length === 0){
-        console.log("Unable to find place")
-
-    }else{
-
-        const latitude = response.body.features[0].center[1];
-        const longitude = response.body.features[0].center[0];
-        console.log(longitude + ", " + latitude);
-    }
-
-})
+});
---
+++ utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const forecast = (longitude, latitude, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=0a9275d4360d2bc6950b120763fbede6&query='+ longitude + ','+ latitude +'&units=m';
+    console.log(url);
+    
+    request({url: url, json: true}, (error, response) =>{
+
+        if (error){
+            callback("Unable to connect to weather services", undefined);
+        }else if(response.body.error){
+            callback("Unable to find coordinates", undefined);
+        }else{
+            const data = response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature+ " degrees out. It feels like " + response.body.current.feelslike+ " degrees out.";  
+            callback(undefined, data);
+        }
+
+
+    });
+
+}
+
+module.exports = forecast;
