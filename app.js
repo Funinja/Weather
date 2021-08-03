cda0d3b28f3421fe579d5dd48a24210965ec00c0
Dennis lam <denwt.lam@outlook.com>
2021-08-03 14:33:43 -0400

    add input functionality of location to find weather

--- app.js
+++ app.js
@@ -2,21 +2,24 @@ const request = require('request');
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
-
-geocode('Boston', (error, data) => {
-
-    if (error){
-        return console.log(error);
-    }
-
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (process.argv[2] === undefined){
+    console.log("There is no location inputted");
+}else{
+    geocode(process.argv[2], (error, data) => {
 
         if (error){
             return console.log(error);
         }
-
-        console.log(data.location);
-        console.log(forecastData);
-    })
-
-});
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+    
+            if (error){
+                return console.log(error);
+            }
+    
+            console.log(data.location);
+            console.log(forecastData);
+        })
+    
+    });
+}
